a0431f03036ecd391e3bac2293e71ea3aa4cde00
Nurana Alili <nurana.alili@outlook.com>
2025-11-04 23:36:59 -0800

    Add files via upload

---
+++ bmi_calculator.py
@@ -0,0 +1,23 @@
+height = float(input("Enter your height in inches: "))
+weight = float(input("Enter your weight in pounds: "))
+
+bmi = (weight * 703)/(height**2)
+if bmi < 16.0:
+    category = "Severely Underweight"
+elif bmi <= 18.4:
+    category = "Underweight"
+elif bmi <= 24.9:
+    category = "Normal"
+elif bmi <= 29.9:
+    category = "Overweight"
+elif bmi <= 34.9:
+    category = "Moderately Obese"
+elif bmi <= 39.9:
+    category = "Severely Obese"
+else: 
+    category = "Morbidly Obese"
+
+bmi = round(bmi, 1)
+
+print(f"Your BMI of {bmi} makes you {category}")
+
